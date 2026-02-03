80836e3e46ac645a29bcc9dc8a5809e9fb2e012f
dityasiddaraj-promantia <ditya.siddaraj@promantia.com>
2026-02-03 13:15:01 +0530

    feat: add FAC welcome email invite

--- frappe_assistant_core/utils/email_invite.py
+++ frappe_assistant_core/utils/email_invite.py
@@ -1,12 +1,9 @@
 import frappe
+
+
 def send_fac_admin_invite():
-    
     users = frappe.get_all(
-        "User",
-        filters={
-            "enabled": 1
-        },
-        fields=["name", "email", "first_name", "full_name"]
+        "User", filters={"enabled": 1}, fields=["name", "email", "first_name", "full_name"]
     )
     recipients = []
 
@@ -18,33 +15,22 @@ def send_fac_admin_invite():
                 recipients.append(user.email)
 
     if not recipients:
-        frappe.log_error(
-            "No System Manager users found for FAC invite",
-            "FAC Invite Hook"
-        )
+        frappe.log_error("No System Manager users found for FAC invite", "FAC Invite Hook")
         return
 
     email_account = frappe.db.get_value(
-        "Email Account",
-        {
-            "default_outgoing": 1,
-            "enable_outgoing": 1
-        },
-        "email_id"
+        "Email Account", {"default_outgoing": 1, "enable_outgoing": 1}, "email_id"
     )
 
     if not email_account:
-        frappe.log_error(
-            "No default outgoing Email Account found",
-            "FAC Invite Hook"
-        )
+        frappe.log_error("No default outgoing Email Account found", "FAC Invite Hook")
         return
 
     frappe.sendmail(
-        recipients=[email_account],  
-        bcc=recipients,               
+        recipients=[email_account],
+        bcc=recipients,
         subject="Welcome to Frappe Assistant Core",
         template="fac_welcome_invite",
         sender=email_account,
-        delayed=False
+        delayed=False,
     )
